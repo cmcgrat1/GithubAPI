be0b421a52dd4dce111642d2201b86e877280d96
Calum McGrath <calummcgrath@Calums-MacBook-Air.local>
2019-11-21 17:47:36 +0000

    Initail analyis: followers, following, num of repositories

--- GithubAccess.R
+++ GithubAccess.R
@@ -1,17 +1,17 @@
-#install.packages("jsonlite")
+install.packages("jsonlite")
 library(jsonlite)
-#install.packages("httpuv")
+install.packages("httpuv")
 library(httpuv)
-#install.packages("httr")
+install.packages("httr")
 library(httr)
 
 # Can be github, linkedin etc depending on application
 oauth_endpoints("github")
 
 # Change based on what you 
-myapp <- oauth_app(appname = "Coursera_John_Hopkins",
-                   key = "8758a6bf9a146e1da0c1",
-                   secret = "b9504edde46b794414495bd9c33ea28cbfd87824")
+myapp <- oauth_app(appname = "Access_Github",
+                   key = "669e0ee8a432c30178f2",
+                   secret = "5d6914b0d869b32a36830cd05e30e946d9a038f9")
 
 # Get OAuth credentials
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
@@ -31,3 +31,28 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # Subset data.frame
 gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+
+-------
+  # Subset data.frame
+  gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+
+#first analysis of my own profile
+myProfile = fromJSON("https://api.github.com/users/cmcgrat1")
+myProfile$followers
+myProfile$public_repos
+
+myFollowers = fromJSON("https://api.github.com/users/cmcgrat1/followers")
+myFollowers$login
+length = length(myFollowers$login)
+length #Number of followers
+
+myProfile$following
+following = fromJSON("https://api.github.com/users/cmcgrat1/following")
+following$login
+
+repository = fromJSON("https://api.github.com/users/cmcgrat1/repos")
+repository$name
+repository$created_at
+
+myProfile$bio
+
