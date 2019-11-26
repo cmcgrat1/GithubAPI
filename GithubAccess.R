9d532937d4430044f264b4e736296d3a344a7f32
cmcgrat1 <48024698+cmcgrat1@users.noreply.github.com>
2019-11-26 13:52:51 +0000

    Update GithubAccess.R
    added plot 1 and plot 2

--- GithubAccess.R
+++ GithubAccess.R
@@ -56,3 +56,113 @@ repository$created_at
 
 myProfile$bio
 
+myProfileJSon = toJSON(myProfile, pretty = TRUE)
+myProfileJSon
+
+
+
+
+#OTHER USER
+# Looking at deviantony who was trending on github
+
+myData = GET("https://api.github.com/users/deviantony/followers?per_page=100;", gtoken)
+stop_for_status(myData)
+extract = content(myData)
+#converts into dataframe
+githubDB = jsonlite::fromJSON(jsonlite::toJSON(extract))
+githubDB$login
+
+# Retrieve a list of usernames
+id = githubDB$login
+user_ids = c(id)
+
+# Create an empty vector and data.frame
+users = c()
+usersDB = data.frame(
+  username = integer(),
+  following = integer(),
+  followers = integer(),
+  repos = integer(),
+  dateCreated = integer()
+)
+
+#loops through users and adds to list
+for(i in 1:length(user_ids))
+{
+  
+  followingURL = paste("https://api.github.com/users/", user_ids[i], "/following", sep = "")
+  followingRequest = GET(followingURL, gtoken)
+  followingContent = content(followingRequest)
+  
+  #Does not add users if they have no followers
+  if(length(followingContent) == 0)
+  {
+    next
+  }
+  
+  followingDF = jsonlite::fromJSON(jsonlite::toJSON(followingContent))
+  followingLogin = followingDF$login
+  
+  #Loop through following users
+  for (j in 1:length(followingLogin))
+  {
+    #Check for duplicate users
+    if (is.element(followingLogin[j], users) == FALSE)
+    {
+      #Adds user to the current list
+      users[length(users) + 1] = followingLogin[j]
+      
+      #Obtain information from each user
+      followingUrl2 = paste("https://api.github.com/users/", followingLogin[j], sep = "")
+      following2 = GET(followingUrl2, gtoken)
+      followingContent2 = content(following2)
+      followingDF2 = jsonlite::fromJSON(jsonlite::toJSON(followingContent2))
+      
+      #Retrieves who user is following
+      followingNumber = followingDF2$following
+      
+      #Retrieves users followers
+      followersNumber = followingDF2$followers
+      
+      #Retrieves number of repository the user has 
+      reposNumber = followingDF2$public_repos
+      
+      #Retrieve year that each user joined Github
+      yearCreated = substr(followingDF2$created_at, start = 1, stop = 4)
+      
+      #Add users data to a new row in dataframe
+      usersDB[nrow(usersDB) + 1, ] = c(followingLogin[j], followingNumber, followersNumber, reposNumber, yearCreated)
+      
+    }
+    next
+  }
+  #Stop when there are more than 10 users
+  if(length(users) > 150)
+  {
+    break
+  }
+  next
+}
+
+#plot 1
+
+#entering my plotly details
+Sys.setenv("plotly_username"="cmcgrat1")
+Sys.setenv("plotly_api_key"="AMSdPCyiiTrFagmA0DFz")
+
+# comparing repositories and followers for ever year 
+plot1 = plot_ly(data = usersDB, x = ~repos, y = ~followers, text = ~paste("Followers: ", followers, "<br>Repositories: ", repos, "<br>Date Created:", dateCreated), color = ~dateCreated)
+plot1
+
+
+#plot 2
+#following vs followers year by year
+plot2 = plot_ly(data = usersDB, x = ~following, y = ~followers, text = ~paste("Followers: ", followers, "<br>Following: ", following), color = ~dateCreated)
+plot2
+
+#upload to plotly
+api_create(plot1, filename = "Repositories vs Followers")
+#upload to plotly
+api_create(plot2, filename = "Following vs Followers") 
+
+
